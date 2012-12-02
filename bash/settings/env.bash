ff1c19843591965c1990f8971bf72713a94e3250
Eric Milford <ericmilford@gmail.com>
2012-12-02 16:40:30 -0500

    [bash] Show git state in prompt

--- bash/settings/env.bash
+++ bash/settings/env.bash
@@ -4,3 +4,5 @@ export PATH="./bin:$HOME/.rbenv/bin:$PATH"
 export HISTCONTROL=ignoredups:erasedups
 shopt -s histappend
 export PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
+
+export GIT_PS1_SHOWDIRTYSTATE=Y
