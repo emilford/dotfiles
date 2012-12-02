4abdc1c7ccf8e2c1309e741c49fd12ca61b978f6
Eric Milford <ericmilford@gmail.com>
2012-12-02 16:39:36 -0500

    [bash] Add common history with no duplicates

--- bash/settings/env.bash
+++ bash/settings/env.bash
@@ -1,1 +1,6 @@
 export PATH="./bin:$HOME/.rbenv/bin:$PATH"
+
+# Prevent duplicates and common history across sessions
+export HISTCONTROL=ignoredups:erasedups
+shopt -s histappend
+export PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
