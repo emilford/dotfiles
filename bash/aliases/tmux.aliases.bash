d721c3d88220971c77e04650e51278a6b2cf167c
Eric Milford <ericmilford@gmail.com>
2013-02-26 18:21:27 -0500

    [bash] Update tmux alias to attach based on current directory

--- bash/aliases/tmux.aliases.bash
+++ bash/aliases/tmux.aliases.bash
@@ -6,7 +6,7 @@ alias tlp='tmux -S /tmp/pair list-sessions'
 alias tdp='tmux -S /tmp/pair detach'
 
 alias tn='ssh-agent tmux new -s `basename $PWD`'
-alias ta='tmux attach -t'
+alias ta='tmux attach -t `basename $PWD`'
 alias ts='tmux switch -t'
 alias tl='tmux list-sessions'
 alias tk='tmux kill-session -t'
