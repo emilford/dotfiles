ec932edaf6a53c093449eb70817a07d09e4e75ca
Eric Milford <ericmilford@gmail.com>
2013-04-13 09:03:38 -0400

    [bash] Add heroku aliases for common commands

---
+++ bash/aliases/heroku.aliases.bash
@@ -0,0 +1,10 @@
+alias hpc='heroku run console --remote production'
+alias hpl='heroku logs --tail --remote production'
+alias hpr='heroku restart --remote production'
+alias hpp='heroku pg:psql --remote production'
+
+alias hsc='heroku run console --remote staging'
+alias hsl='heroku logs --tail --remote staging'
+alias hsr='heroku restart --remote staging'
+alias hsp='heroku pg:psql --remote staging'
+
