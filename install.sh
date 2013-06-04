76578d2ec78b31bd778bbead6756f9f4e3ba694e
Eric Milford <ericmilford@gmail.com>
2013-06-04 18:34:07 -0400

    [bash] Update install script for auto linking

--- install.sh
+++ install.sh
@@ -1,16 +1,7 @@
-ln -s /Users/Eric/.dotfiles/bash/bash_profile ~/.bash_profile
-ln -s /Users/Eric/.dotfiles/ruby/gemrc ~/.gemrc
-ln -s /Users/Eric/.dotfiles/git/gitconfig ~/.gitconfig
-ln -s /Users/Eric/.dotfiles/git/githelpers ~/.githelpers
-ln -s /Users/Eric/.dotfiles/git/gitignore ~/.gitignore
-ln -s /Users/Eric/.dotfiles/git/gittemplate ~/.gittemplate
-ln -s /Users/Eric/.dotfiles/vim ~/.vim
-ln -s /Users/Eric/.dotfiles/vim/vimrc ~/.vimrc
-ln -s /Users/Eric/.dotfiles/vim/gist-vim ~/.gist-vim
-ln -s /Users/Eric/.dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -s /Users/Eric/.dotfiles/rbenv/plugins/ruby-build $RBENV_ROOT/plugins/ruby-build
-ln -s /Users/Eric/.dotfiles/rbenv/plugins/rbenv-default-gesm $RBENV_ROOT/plugins/rbenv-default-gesm
-ln -s /Users/Eric/.dotfiles/rbenv/default-gems $RBENV_ROOT/default-gems
-ln -s /Users/Eric/.dotfiles/rbenv/plugins/rbenv-aliases $RBENV_ROOT/plugins/rbenv-aliases
-ln -s /Users/Eric/.dotfiles/rbenv/plugins/rbenv-communal-gems $RBENV_ROOT/plugins/rbenv-communal-gems
-ln -s /Users/Eric/.dotfiles/rbenv/plugins/rbenv-ctags $RBENV_ROOT/plugins/rbenv-ctags
+for f in `find ~/.dotfiles -name '*.ln'`; do
+  filename=$(basename "$f")
+
+  if [ ! -e "$HOME/.${filename%.ln}" ]; then
+    ln -s $f "$HOME/.${filename%.ln}"
+  fi
+done
