85ca54081092dc75ce47d0dba1ed46d253e8e51c
Eric Milford <ericmilford@gmail.com>
2013-06-04 16:28:22 -0400

    [bash] Update tmux aliases to accept optional session name

--- bash/aliases/tmux.aliases.bash
+++ bash/aliases/tmux.aliases.bash
@@ -1,6 +1,18 @@
-alias tn='ssh-agent tmux -S /var/tmux/session new -s `basename $PWD`'
-alias ta='tmux -S /var/tmux/session attach -t `basename $PWD`'
-alias ts='tmux -S /var/tmux/session switch -t'
 alias tl='tmux -S /var/tmux/session list-sessions'
-alias tk='tmux -S /var/tmux/session kill-session -t'
 alias td='tmux -S /var/tmux/session detach'
+
+function tn () {
+  $(ssh-agent tmux -S /var/tmux/session new -s ${1-${PWD##*/}})
+}
+
+function ta () {
+  $(tmux -S /var/tmux/session attach -t ${1-${PWD##*/}})
+}
+
+function tk () {
+  $(tmux -S /var/tmux/session kill-session -t ${1-${PWD##*/}})
+}
+
+function ts () {
+  $(tmux -S /var/tmux/session switch -t ${1-${PWD##*/}})
+}
