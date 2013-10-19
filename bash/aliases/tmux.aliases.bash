d9608dd8f6c2afbec9ce205065c39381f04d81cf
Eric Milford <ericmilford@gmail.com>
2013-10-18 20:17:13 -0400

    [bash] Use ~/.tmux.socket for tmux sessions

--- bash/aliases/tmux.aliases.bash
+++ bash/aliases/tmux.aliases.bash
@@ -1,18 +1,18 @@
-alias tl='tmux -S /var/tmux/session list-sessions'
-alias td='tmux -S /var/tmux/session detach'
+alias tl='tmux -S ~/.tmux.socket list-sessions'
+alias td='tmux -S ~/.tmux.socket detach'
 
 function tn () {
-  $(ssh-agent tmux -S /var/tmux/session new -s ${1-${PWD##*/}})
+  $(ssh-agent tmux -S ~/.tmux.socket new -s ${1-${PWD##*/}})
 }
 
 function ta () {
-  $(tmux -S /var/tmux/session attach -t ${1-${PWD##*/}})
+  $(tmux -S ~/.tmux.socket attach -t ${1-${PWD##*/}})
 }
 
 function tk () {
-  $(tmux -S /var/tmux/session kill-session -t ${1-${PWD##*/}})
+  $(tmux -S ~/.tmux.socket kill-session -t ${1-${PWD##*/}})
 }
 
 function ts () {
-  $(tmux -S /var/tmux/session switch -t ${1-${PWD##*/}})
+  $(tmux -S ~/.tmux.socket switch -t ${1-${PWD##*/}})
 }
--- bash/settings/ssh.bash
+++ bash/settings/ssh.bash
@@ -1,6 +1,6 @@
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
   session_names=()
-  while IFS=: read -r session_name _; do session_names+=("$session_name"); done < <(tmux -S /var/tmux/session list-sessions)
+  while IFS=: read -r session_name _; do session_names+=("$session_name"); done < <(tmux -S ~/.tmux.socket list-sessions)
 
   for i in "${!session_names[@]}"; do
     printf "[%s] %s  " "$((i+1))" "${session_names[$i]}"
@@ -10,5 +10,5 @@ if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
 
   read session
 
-  exec tmux -S /var/tmux/session attach -t ${session_names[$((session-1))]}
+  exec tmux -S ~/.tmux.socket attach -t ${session_names[$((session-1))]}
 fi
