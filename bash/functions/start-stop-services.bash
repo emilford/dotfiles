ac2c6fc4516a49250979f0d77ad2a14027e265a0
Eric Milford <ericmilford@gmail.com>
2014-11-24 12:41:20 -0500

    Rename start/stop service helpers

--- bash/functions/start-stop-services.bash
+++ bash/functions/start-stop-services.bash
@@ -1,4 +1,4 @@
-start() {
+start-service() {
   case "$1" in
     mysql) 
       mysql.server start
@@ -24,7 +24,7 @@ start() {
   esac
 }
 
-stop() {
+stop-service() {
   case "$1" in
     mysql)
       mysql.server stop
