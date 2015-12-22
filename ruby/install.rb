0a332ba36cc73f6052a39513708a575c4b604276
Eric Milford <ericmilford@gmail.com>
2015-12-22 13:04:47 -0500

    Only install ruby versions if they don't already exist

--- ruby/install.rb
+++ ruby/install.rb
@@ -12,9 +12,21 @@ def main
   symlink("~/Code/dotfiles/ruby/rspec", "~/.rspec")
   symlink("~/Code/dotfiles/ruby/ruby-version", "~/.ruby-version")
 
-  run("ruby-install ruby 2.2.4", "Ruby install 2.2.4")
-  run("ruby-install ruby 2.2.3", "Ruby install 2.2.3")
+  ruby_install_version("2.2.4")
+  ruby_install_version("2.2.3")
   run("rm -rf ~/src")
+end
+
+def ruby_install_version(version)
+  if ruby_version_installed?(version)
+    success("ruby-install #{version}")
+  else
+    run("ruby-install ruby #{version}", "ruby-install #{version}")
+  end
+end
+
+def ruby_version_installed?(version)
+  Dir.exists?(File.expand_path("~/.rubies/ruby-#{version}"))
 end
 
 main
