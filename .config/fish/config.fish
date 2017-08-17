c20b888d87389c21d0294324dcd4a3ede0904d59
Eric Milford <ericmilford@gmail.com>
2017-08-16 23:27:07 -0400

    [fish] Add tmux abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,5 +10,11 @@ abbr be bundle exec
 abbr bi bundle install
 abbr bu bundle udpate
 
+abbr ta tmux attach -t
+abbr td tmux detach
+abbr tk tmux kill-session -t
+abbr tl tmux list-sessions
+abbr tn tmux new -s
+
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
