a5ed9596c865270bae1dcaa56be8e59b08783e28
Eric Milford <ericmilford@gmail.com>
2017-08-16 21:38:19 -0400

    [fish] Source enable chruby

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,6 @@ set -gx PATH $HOME/.local/bin $PATH
 set -gx PATH .git/safe/../../bin $PATH
 
 set -x CDPATH $CDPATH $HOME/Code
+
+source /usr/local/share/chruby/chruby.fish
+source /usr/local/share/chruby/auto.fish
