11f4ed51c8ce6caf85b3e72fbc5e5dc8d66e95b1
Eric Milford <ericmilford@gmail.com>
2017-08-16 23:27:45 -0400

    [fish] Add SSH remote login tmux menu

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,3 +18,15 @@ abbr tn tmux new -s
 
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
+
+if test -n "$SSH_CLIENT" ; or test -n "$SSH_TTY"
+  set sessions (tmux list-sessions -F "#{session_name}")
+
+  for i in (seq (count $sessions))
+    echo [$i] $sessions[$i]
+  end
+
+  read -p 'echo -n "Enter the number of the tmux session to join: "' -l selected
+
+  exec tmux attach -t $sessions[$selected]
+end
