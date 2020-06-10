05b07c1dc108498a9e16c6c2181951cd5ae5aadf
Eric Milford <ericmilford@gmail.com>
2020-06-10 17:22:49 -0500

    fish: update dotfiles function to use git on PATH

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,4 +1,4 @@
 function dotfiles -w git -d "Interact with dotfiles bare repo"
-  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
+  git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
 end
 
