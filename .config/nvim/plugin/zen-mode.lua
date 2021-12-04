b9d9c94caea2f15c5b4535a68175a47a6be90112
Eric Milford <ericmilford@gmail.com>
2021-12-04 08:48:45 -0600

    (nvim) Add Zen Mode for a distraction-free editor
    For the times where focus on a single thing is needed, disable all of
    the extras in Neovim and Tmux, and bump the font size. The ability to
    increase the font size requires that Kitty listen for commands on the
    specified socket, and that Tmux also has the correct socket set for
    communication with Kitty.

---
+++ .config/nvim/plugin/zen-mode.lua
@@ -0,0 +1,26 @@
+require("zen-mode").setup({
+	window = {
+		backdrop = 1,
+		width = 80,
+		options = {
+			signcolumn = "no",
+			number = false,
+			cursorline = false,
+			cursorcolumn = false,
+		},
+	},
+	plugins = {
+		options = {
+			enabled = true,
+			ruler = false,
+			showcmd = false,
+		},
+		tmux = {
+			enabled = true,
+		},
+		kitty = {
+			enabled = true,
+			font = "+2",
+		},
+	},
+})
