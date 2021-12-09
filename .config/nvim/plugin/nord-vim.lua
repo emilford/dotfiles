8c939ff1b4d064238070a49e86aa042a60f1067a
Eric Milford <ericmilford@gmail.com>
2021-12-09 16:36:41 -0600

    (nvim) Highlight ERB tag delimiters
    Apply Nord styles to ERB tag delimiters to make them easier to see. An
    outstanding issue on the nord-vim repository is being tracked. Once it
    is merged, this change will become unnecessary and can be removed.

--- .config/nvim/plugin/nord-vim.lua
+++ .config/nvim/plugin/nord-vim.lua
@@ -1,3 +1,6 @@
 vim.g.nord_italic = 1
 
+-- https://github.com/arcticicestudio/nord-vim/issues/279
+vim.cmd([[ autocmd ColorScheme nord :highlight erubyDelimiter ctermbg=5 guifg=#B48EAD ]])
+
 vim.cmd([[colorscheme nord]])
