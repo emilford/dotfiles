f9a58e808d947efdac79178a6c7a8a56543f2604
Eric Milford <ericmilford@gmail.com>
2021-12-28 16:30:43 -0500

    (nvim) Install default set of language servers
    To avoid needing to manually install certain language servers, for any
    language servers that are used regularly, install them automatically.

--- .config/nvim/lua/em/lsp/init.lua
+++ .config/nvim/lua/em/lsp/init.lua
@@ -41,9 +41,34 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
+local lsp_installer = require("nvim-lsp-installer")
+
+-- Include the servers you want to have installed by default below
+local servers = {
+	"cssls",
+	"emmet_ls",
+	"eslint",
+	"html",
+	"jsonls",
+	"solargraph",
+	"sumneko_lua",
+	"tailwindcss",
+	"tsserver",
+	"yamlls",
+}
+
+for _, name in pairs(servers) do
+	local server_is_found, server = lsp_installer.get_server(name)
+	if server_is_found then
+		if not server:is_installed() then
+			print("Installing " .. name)
+			server:install()
+		end
+	end
+end
+
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
-local lsp_installer = require("nvim-lsp-installer")
 lsp_installer.on_server_ready(function(server)
 	local opts = {
 		on_attach = on_attach,
