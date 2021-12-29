2974cc1802af76c74a588643040d7b4607d4e20c
Eric Milford <ericmilford@gmail.com>
2021-12-28 22:00:05 -0500

    (nvim) Disable Solargraph formatting
    Solargraph formatting is experimental and when enabled requires picking
    between it and null-ls' standardrb when formatting. Since most projects
    rely on standardrb for Ruby style, this sets it as the only formatter.

--- .config/nvim/lua/em/lsp/init.lua
+++ .config/nvim/lua/em/lsp/init.lua
@@ -82,6 +82,12 @@ lsp_installer.on_server_ready(function(server)
 		capabilities = capabilities,
 	}
 
+	if server.name == "solargraph" then
+		opts.init_options = {
+			formatting = false,
+		}
+	end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
