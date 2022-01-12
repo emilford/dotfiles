bad20649162a37a10d866cb01e54130f84b0e3e6
Eric Milford <ericmilford@gmail.com>
2022-01-12 17:07:54 -0600

    (nvim) Remove ctrl-p mapping for finding files
    The `ctrl-p` mapping is a leftover remnant from original using a library
    of the same name. The mapping sometimes conflicts with built-in
    functionality, however, (see `h: ctrl-p`).

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -15,12 +15,6 @@ require("telescope").load_extension("fzf")
 
 vim.api.nvim_set_keymap(
 	"n",
-	"<c-p>",
-	[[<cmd> lua require("telescope.builtin").git_files()<cr>]],
-	{ noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-	"n",
 	"<leader>ff",
 	[[<cmd> lua require("telescope.builtin").git_files()<cr>]],
 	{ noremap = true, silent = true }
