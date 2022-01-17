ccfc846d73b71525c2b0a9508f10427a93628000
Eric Milford <ericmilford@gmail.com>
2022-01-16 19:45:52 -0600

    (nvim) Use zk and zk-nvim for note taking
    `zk` is a command-line plain text note-taking tool that's useful for
    keeping a Zettelkasten.

---
+++ .config/nvim/plugin/zk-nvim.lua
@@ -0,0 +1,39 @@
+require("zk").setup({
+	picker = "telescope",
+})
+
+-- Add the key mappings only when in a zk notebook
+if require("zk.util").notebook_root(vim.fn.getcwd()) ~= nil then
+	vim.api.nvim_set_keymap("n", "<cr>", "<cmd>lua vim.lsp.buf.definition()<cr>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>zb", "<cmd>ZkBacklinks<cr>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>zf", "<cmd>ZkNotes<cr>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap(
+		"n",
+		"<leader>zF",
+		"<cmd>ZkNotes { dir = vim.fn.expand('%:p:h'), match = vim.fn.input('Search: ') }<cr>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap("v", "<leader>zf", ":'<'>ZkMatch<cr>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>zi", "<cmd>ZkIndex<cr>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>zl", "<cmd>ZkLinks<cr>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap(
+		"n",
+		"<leader>zn",
+		"<cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<cr>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"v",
+		"<leader>zn",
+		":'<'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<cr>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"v",
+		"<leader>zN",
+		":'<'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') } <cr>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap("n", "<leader>zt", "<cmd>ZkTags<cr>", { noremap = true, silent = true })
+end
