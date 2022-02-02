48744478073d7b8bae6e84789049c8665fadf758
Eric Milford <ericmilford@gmail.com>
2022-02-02 16:04:04 -0600

    (nvim) Add quick navigation for frequent
    Harpoon provides functionality for easily and efficiently navigating
    between a small set of frequently-visited files. It's similar to the
    built-in global marks functionality, with a few useful enhancements.

---
+++ .config/nvim/plugin/harpoon.lua
@@ -0,0 +1,6 @@
+vim.api.nvim_set_keymap("n", "<leader>h", ":lua require('harpoon.ui').toggle_quick_menu()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>a", ":lua require('harpoon.mark').add_file()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>1", ":lua require('harpoon.ui').nav_file(1)<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>2", ":lua require('harpoon.ui').nav_file(2)<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>3", ":lua require('harpoon.ui').nav_file(3)<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>4", ":lua require('harpoon.ui').nav_file(4)<cr>", { noremap = true })
