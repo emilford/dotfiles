ccb7b9d6102ffa120b3158d4f34b01db7d22aaad
Eric Milford <ericmilford@gmail.com>
2022-03-19 19:48:33 -0500

    (nvim) Set options with vim.opt instead of vim.o
    Using `vim.opt` provides a more robust, full-featured API for setting
    and controlling options and that simplifies setting options and gives
    the expected result.

--- .config/nvim/lua/em/options/init.lua
+++ .config/nvim/lua/em/options/init.lua
@@ -1,15 +1,15 @@
 vim.g.mapleader = ","
 
-vim.o.colorcolumn = "+1"
-vim.o.cpoptions = vim.o.cpoptions .. "$"
-vim.o.diffopt = vim.o.diffopt .. ",vertical"
-vim.o.hidden = true
-vim.o.joinspaces = false
-vim.o.laststatus = 1
-vim.o.number = true
-vim.o.ruler = false
-vim.o.shiftround = true
-vim.o.shortmess = vim.o.shortmess .. "I"
-vim.o.showmatch = true
-vim.o.termguicolors = true
-vim.o.wildmode = "list:longest,list:full"
+vim.opt.colorcolumn = "+1"
+vim.opt.cpoptions:append("$")
+vim.opt.diffopt:append("vertical")
+vim.opt.hidden = true
+vim.opt.joinspaces = false
+vim.opt.laststatus = 1
+vim.opt.number = true
+vim.opt.ruler = false
+vim.opt.shiftround = true
+vim.opt.shortmess:append("I")
+vim.opt.showmatch = true
+vim.opt.termguicolors = true
+vim.opt.wildmode = { "list:longest", "list:full" }
