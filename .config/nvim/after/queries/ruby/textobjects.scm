964b17327e1f3548646b6b2d652b692e1f4c2573
Eric Milford <ericmilford@gmail.com>
2022-03-25 23:12:36 -0500

    (nvim) Extend Ruby method definition text object
    The text object queries in `nvim-treesitter-textobject` do not select
    class methods or singleton methods. Adding these locally to test out,
    with the intention of then opening a PR to add them upstream.

---
+++ .config/nvim/after/queries/ruby/textobjects.scm
@@ -0,0 +1,2 @@
+((singleton_method . object: (_) name: (identifier) (method_parameters)? . (_) @function.inner (_)? @function.end .)
+   (#make-range! "function.inner" @function.inner @function.end)) @function.outer
