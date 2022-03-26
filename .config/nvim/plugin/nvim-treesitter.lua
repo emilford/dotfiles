5b7d9a4c517d72e55e27ea1297cc6bc59936eae9
Eric Milford <ericmilford@gmail.com>
2022-03-25 23:05:38 -0500

    (nvim) Enable commonly used select text objects
    This enables text object selection for classes, methods, and blocks,
    with pneumonic key mappings. This did not enable the parameter text
    object in favor of using Neovim's built-in argument text object:
    `h: targets-argument-text-objects`.

--- .config/nvim/plugin/nvim-treesitter.lua
+++ .config/nvim/plugin/nvim-treesitter.lua
@@ -4,4 +4,18 @@ require("nvim-treesitter.configs").setup({
     enable = true,
     additional_vim_regex_highlighting = true,
   },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["ab"] = "@block.outer",
+        ["ib"] = "@block.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+      },
+    },
+  },
 })
