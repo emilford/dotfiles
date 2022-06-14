56203b572d171c7d17020078b00c798516d11f69
Eric Milford <ericmilford@gmail.com>
2022-06-14 11:29:35 -0500

    (nvim) Use which-key for notes mappings
    Reading, writing, and finding notes uses a collection of key mappings
    that aren't always easy to recall. When this happens, remembering the
    "z" (for Zettelkasten) pneumonic launches which-key for access to the
    full list of notes key mappings with labels describing their behavior.

--- .config/nvim/lua/em/plugins/config/zk-nvim.lua
+++ .config/nvim/lua/em/plugins/config/zk-nvim.lua
@@ -7,38 +7,35 @@ function M.config()
 
   -- Add the key mappings only when in a zk notebook
   if require("zk.util").notebook_root(vim.fn.getcwd()) ~= nil then
+    local wk = require("which-key")
+
     vim.keymap.set("n", "<cr>", "<cmd>lua vim.lsp.buf.definition()<cr>", { silent = true })
     vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", { silent = true })
-    vim.keymap.set("n", "<leader>zb", "<cmd>ZkBacklinks<cr>", { silent = true })
-    vim.keymap.set("n", "<leader>zf", "<cmd>ZkNotes<cr>", { silent = true })
-    vim.keymap.set(
-      "n",
-      "<leader>zF",
-      "<cmd>ZkNotes { dir = vim.fn.expand('%:p:h'), match = vim.fn.input('Search: ') }<cr>",
-      { silent = true }
-    )
-    vim.keymap.set("v", "<leader>zf", ":'<'>ZkMatch<cr>", { silent = true })
-    vim.keymap.set("n", "<leader>zi", "<cmd>ZkIndex<cr>", { silent = true })
-    vim.keymap.set("n", "<leader>zl", "<cmd>ZkLinks<cr>", { silent = true })
-    vim.keymap.set(
-      "n",
-      "<leader>zn",
-      "<cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<cr>",
-      { silent = true }
-    )
-    vim.keymap.set(
-      "v",
-      "<leader>zn",
-      ":'<'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<cr>",
-      { silent = true }
-    )
-    vim.keymap.set(
-      "v",
-      "<leader>zN",
-      ":'<'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') } <cr>",
-      { silent = true }
-    )
-    vim.keymap.set("n", "<leader>zt", "<cmd>ZkTags<cr>", { silent = true })
+
+    wk.register({
+      z = {
+        name = "+ZettelKasten",
+        b = { "<cmd>ZkBacklinks<cr>", "Backlinks" },
+        o = { "<cmd>ZkNotes<cr>", "Open" },
+        f = { "<cmd>ZkNotes { dir = vim.fn.expand('%:p:h'), match = vim.fn.input('Find: ') }<cr>", "Find" },
+        i = { "<cmd>ZkIndex<cr>", "Index" },
+        l = { "<cmd>ZkLinks<cr>", "Links" },
+        n = { "<cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<cr>", "New" },
+        t = { "<cmd>ZkTags<cr>", "Tags" },
+      },
+    }, { prefix = "<leader>", mode = "n" })
+
+    wk.register({
+      z = {
+        name = "+ZettelKasten",
+        f = { ":'<'>ZkMatch<cr>", "Find" },
+        n = { ":'<'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<cr>", "New" },
+        N = {
+          ":'<'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') } <cr>",
+          "New",
+        },
+      },
+    }, { prefix = "<leader>", mode = "v" })
   end
 end
 
