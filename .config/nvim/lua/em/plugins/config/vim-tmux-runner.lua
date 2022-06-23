27fd103bf791360bcbc06a975acf4cf629e88e07
Eric Milford <ericmilford@gmail.com>
2022-06-22 19:46:14 -0500

    (nvim) Use <leader> for tmux runner key mappings
    These key mappings have historically used the <leader> key. Without the
    <leader> prefix, the key mapping conflicts with the built-in `r` motion
    which leads to undesired behavior.

--- .config/nvim/lua/em/plugins/config/vim-tmux-runner.lua
+++ .config/nvim/lua/em/plugins/config/vim-tmux-runner.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 function M.config()
-  vim.keymap.set("n", "ra", ":VtrAttachToPane<cr>")
-  vim.keymap.set("n", "rc", ":VtrClearRunner<cr>")
-  vim.keymap.set("n", "rf", ":VtrFocusRunner<cr>")
-  vim.keymap.set("n", "ro", ":VtrOpenRunner<cr>")
-  vim.keymap.set("n", "rk", ":VtrKillRunner<cr>")
+  vim.keymap.set("n", "<leader>ra", ":VtrAttachToPane<cr>")
+  vim.keymap.set("n", "<leader>rc", ":VtrClearRunner<cr>")
+  vim.keymap.set("n", "<leader>rf", ":VtrFocusRunner<cr>")
+  vim.keymap.set("n", "<leader>ro", ":VtrOpenRunner<cr>")
+  vim.keymap.set("n", "<leader>rk", ":VtrKillRunner<cr>")
 end
 
 return M
