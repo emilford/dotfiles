ca7d6d807a195350f08398d24c019c183294a022
Eric Milford <ericmilford@gmail.com>
2022-06-22 19:46:44 -0500

    (nvim) Fix running visually selected SQL
    vim-dadbod has preliminary map support and allows defining key mappings
    to execute SQL using normal mode and visual mode. This is handy for SQL
    under the cursor or a SQL file. To be able to run the visually selected
    SQL, this fixes one of the key mappings to be an `xmap` key mapping.

--- .config/nvim/lua/em/plugins/config/vim-dadbod.lua
+++ .config/nvim/lua/em/plugins/config/vim-dadbod.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.config()
   vim.keymap.set("n", "<C-Q>", "db#op_exec()", { expr = true })
-  vim.keymap.set("n", "<C-Q>", "db#op_exec()", { expr = true })
+  vim.keymap.set("x", "<C-Q>", "db#op_exec()", { expr = true })
 end
 
 return M
