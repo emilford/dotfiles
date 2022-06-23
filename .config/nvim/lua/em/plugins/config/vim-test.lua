83f2a1d74178349e4e509f263832b20bee9b4dd6
Eric Milford <ericmilford@gmail.com>
2022-06-22 19:39:24 -0500

    (nvim) Use <leader> for test key mappings
    These key mappings have historically used the <leader> key. Without the
    <leader> prefix, the key mapping conflicts with the built-in `t` motion
    which leads to undesired behavior.

--- .config/nvim/lua/em/plugins/config/vim-test.lua
+++ .config/nvim/lua/em/plugins/config/vim-test.lua
@@ -3,11 +3,11 @@ local M = {}
 function M.config()
   vim.g["test#strategy"] = "vtr"
 
-  vim.keymap.set("n", "tn", ":TestNearest<cr>", { silent = true })
-  vim.keymap.set("n", "tf", ":TestFile<cr>", { silent = true })
-  vim.keymap.set("n", "ta", ":TestSuite<cr>", { silent = true })
-  vim.keymap.set("n", "tl", ":TestLast<cr>", { silent = true })
-  vim.keymap.set("n", "tv", ":TestVisit<cr>", { silent = true })
+  vim.keymap.set("n", "<leader>tn", ":TestNearest<cr>", { silent = true })
+  vim.keymap.set("n", "<leader>tf", ":TestFile<cr>", { silent = true })
+  vim.keymap.set("n", "<leader>ta", ":TestSuite<cr>", { silent = true })
+  vim.keymap.set("n", "<leader>tl", ":TestLast<cr>", { silent = true })
+  vim.keymap.set("n", "<leader>tv", ":TestVisit<cr>", { silent = true })
 end
 
 return M
