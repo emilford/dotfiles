7ea3527e9cc63e5a0bf74b9bd2bdcad7530ce06c
Eric Milford <ericmilford@gmail.com>
2022-07-08 13:29:11 -0500

    (nvim) Extend Nord with cmp highlights
    The original Nord color scheme has improved its support for Neovim, but
    is missing highlight groups for some plugins. This extends support with
    highlights for nvim-cmp to better distinguish pop up content.

--- .config/nvim/lua/em/plugins/config/nord-vim.lua
+++ .config/nvim/lua/em/plugins/config/nord-vim.lua
@@ -9,6 +9,34 @@ function M.config()
   -- https://www.reddit.com/r/neovim/comments/uhv6xs/is_it_possible_to_make_lsp_hover_windows/
   vim.cmd([[autocmd ColorScheme nord :highlight! link NormalFloat Normal]])
 
+  local nord = require("em.themes.nord.palette")
+
+  local highlight = function(group, arguments)
+    arguments.style = arguments.style and "gui=" .. arguments.style or "gui=NONE"
+    arguments.fg = arguments.fg and "guifg=" .. arguments.fg or "guifg=NONE"
+    arguments.bg = arguments.bg and "guibg=" .. arguments.bg or "guibg=NONE"
+    arguments.sp = arguments.sp and "guisp=" .. arguments.sp or ""
+
+    vim.cmd(
+      "autocmd ColorScheme nord :highlight "
+        .. group
+        .. " "
+        .. arguments.style
+        .. " "
+        .. arguments.fg
+        .. " "
+        .. arguments.bg
+        .. " "
+        .. arguments.sp
+    )
+  end
+
+  highlight("CmpItemAbbr", { fg = nord.gui.nord4 })
+  highlight("CmpItemAbbrMatch", { fg = nord.gui.nord7, style = "bold" })
+  highlight("CmpItemAbbrMatchFuzzy", { fg = nord.gui.nord7, style = "bold" })
+  highlight("CmpItemKind", { fg = nord.gui.nord15 })
+  highlight("CmpItemMenu", { fg = nord.gui.nord14 })
+
   vim.cmd([[colorscheme nord]])
 end
 
