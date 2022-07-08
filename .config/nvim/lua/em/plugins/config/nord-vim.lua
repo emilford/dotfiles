83b352accd657ad4e07a2a4e5e36ae9012b0193f
Eric Milford <ericmilford@gmail.com>
2022-07-07 21:29:25 -0500

    (nvim) Use Normal highlight group for pop ups
    Nord adds a background color for the `NormalFloat` highlight group used
    in the completion and hover pop ups. `Normal` uses Nord's default color
    for text and a transparent background. This keeps the pop ups minimal.

--- .config/nvim/lua/em/plugins/config/nord-vim.lua
+++ .config/nvim/lua/em/plugins/config/nord-vim.lua
@@ -6,6 +6,9 @@ function M.config()
   -- https://github.com/arcticicestudio/nord-vim/issues/279
   vim.cmd([[autocmd ColorScheme nord :highlight erubyDelimiter ctermbg=5 guifg=#B48EAD]])
 
+  -- https://www.reddit.com/r/neovim/comments/uhv6xs/is_it_possible_to_make_lsp_hover_windows/
+  vim.cmd([[autocmd ColorScheme nord :highlight! link NormalFloat Normal]])
+
   vim.cmd([[colorscheme nord]])
 end
 
