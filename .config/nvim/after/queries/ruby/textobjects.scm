abf6bc2319ba2d54de55a8a7bf8e8023edc99292
Eric Milford <ericmilford@gmail.com>
2022-08-30 05:44:57 -0500

    (nvim) Remove custom Ruby text object
    The text object queries in `nvim-treesitter-textobject` did not select
    class methods or singleton methods. This custom Ruby text object fixed
    the issue, but is no longer needed after updates made in the plugin to
    the Ruby queries.

--- .config/nvim/after/queries/ruby/textobjects.scm
+++
@@ -1,2 +0,0 @@
-((singleton_method . object: (_) name: (identifier) (method_parameters)? . (_) @function.inner (_)? @function.end .)
-   (#make-range! "function.inner" @function.inner @function.end)) @function.outer
