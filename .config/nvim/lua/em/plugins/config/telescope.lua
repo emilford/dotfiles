66489bb759bdb8a3cb35244ea2ff033afa5b597f
Eric Milford <ericmilford@gmail.com>
2022-10-09 15:18:01 -0500

    (nvim) Do not hide Telescope preview by default
    Showing the preview while searching files isn't helpful but it may be
    helpful for other types of Telescope searches. Instead of disabling
    previews by default, set it a per picker.

--- .config/nvim/lua/em/plugins/config/telescope.lua
+++ .config/nvim/lua/em/plugins/config/telescope.lua
@@ -8,18 +8,17 @@ function M.config()
           ["<c-\\>"] = require("telescope.actions.layout").toggle_preview,
         },
       },
-      preview = {
-        hide_on_startup = true,
-      },
     },
   })
 
-  vim.keymap.set(
-    "n",
-    "<leader>ff",
-    [[<cmd> lua require("telescope.builtin").git_files()<cr>]],
-    { silent = true }
-  )
+  vim.keymap.set("n", "<leader>ff", function()
+    require("telescope.builtin").git_files({
+      preview = {
+        hide_on_startup = true,
+      },
+    })
+  end, { silent = true })
+
 end
 
 return M
