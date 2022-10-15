c4fa5f66ca0c1ff7916d977dae5b65dad3976224
Eric Milford <ericmilford@gmail.com>
2022-10-15 12:33:21 -0500

    (nvim) Trigger `WhichKey` on `<leader>`
    `vim-rails` extends `gf` with go to file support for Rails codebases but
    does so if a `gf` map doesn't already exist. It checks for this with the
    `mapcheck('gf', ...)` command, which matches mappings that match the
    start of `gf` (.i.e., `gf` matches on `g`).
    
    `WhichKey` defines mappings for presets including mappings for commands
    prefixed with "g". This causes an issue because the presence of the `g`
    map causes `vim-rails` not to extend `gf` with Rails support.
    
    Options exist to disable mappings for `g` and other presets, but doing
    this doesn't remove the `g` map. Since custom mappings all start with
    `<leader>` and extending `gf`'s more important than preset mappings,
    trigger `WhichKey` specifically for `<leader>` mappings.

--- .config/nvim/lua/em/plugins/config/which-key.lua
+++ .config/nvim/lua/em/plugins/config/which-key.lua
@@ -1,7 +1,10 @@
 local M = {}
 
 function M.config()
-  require("which-key").setup({ ignore_missing = true })
+  require("which-key").setup({
+    ignore_missing = true,
+    triggers = { "<leader>" },
+  })
 end
 
 return M
