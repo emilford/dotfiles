f5482e942cb564553c58dcb88688406b36411ce2
Eric Milford <ericmilford@gmail.com>
2023-01-11 21:13:18 -0600

    (nvim) Remove explicit filetype.lua
    Neovim 0.8+ enabled filetype.lua by default. Now running 0.8+, this
    guard and the settings for Neovim 0.7 are no longer needed.

--- .config/nvim/filetype.lua
+++
@@ -1,9 +0,0 @@
--- Enabling filetype.lua is the default in 0.8. So for that
--- version we can remove these lines, but I don't see a way
--- to check for versions >= to, and has("nvim-0.7") is true
--- for 0.8 as well.
-if vim.fn.has("nvim-0.8") == 1 then
-else
-  vim.g.do_filetype_lua = 1
-  vim.g.did_load_filetypes = 0
-end
