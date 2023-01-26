69d896c10882495c8ccf7eb377935eb2f8e0183e
Eric Milford <ericmilford@gmail.com>
2023-01-25 22:07:17 -0600

    (nvim) Lazy load autocmds and keymaps
    When opening Neovim with no files in the argument list, we can delay
    loading autocmds and keymaps. When opened with files, load them on
    init to make them available to opened buffers.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,17 @@
 require("config.options")
-require("config.keymaps")
 require("config.lazy")
-require("config.autocmds")
+
+if vim.fn.argc() == 0 then
+  -- autocmds and keymaps can wait to load
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "VeryLazy",
+    callback = function()
+      require("config.autocmds")
+      require("config.keymaps")
+    end,
+  })
+else
+  -- load them now so they affect the opened buffers
+  require("config.autocmds")
+  require("config.keymaps")
+end
