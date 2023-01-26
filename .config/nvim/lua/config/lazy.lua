b6a5d62bd80922f92d0603a69efea4c3466138ac
Eric Milford <ericmilford@gmail.com>
2023-01-25 20:33:51 -0600

    (nvim) Move Lazy configuration to top level
    The pattern of using a custom subdirectory seems unnecessary with Lazy.
    Fine to keep, but might as well remove the unnecessary directory.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -14,35 +14,4 @@ end
 
 vim.opt.runtimepath:prepend(lazypath)
 
-require("lazy").setup({
-  spec = {
-    { import = "em.plugins" },
-    { import = "em.plugins.config.splitjoin" },
-    { import = "em.plugins.config.nvim-colorizer" },
-    { import = "em.plugins.config.nvim-treesitter" },
-    { import = "em.plugins.config.harpoon" },
-    { import = "em.plugins.config.rose-pine" },
-    { import = "em.plugins.config.ultisnips" },
-    { import = "em.plugins.config.vim-tmux-runner" },
-    { import = "em.plugins.config.zen-mode" },
-    { import = "em.plugins.config.vim-test" },
-    { import = "em.plugins.config.fidget" },
-    { import = "em.plugins.config.cmp" },
-    { import = "em.plugins.config.vim-easy-align" },
-    { import = "em.plugins.config.indent-blankline" },
-    { import = "em.plugins.config.vim-dirtytalk" },
-    { import = "em.plugins.config.vim-fugitive" },
-    { import = "em.plugins.config.loupe" },
-    { import = "em.plugins.config.nvim-autopairs" },
-    { import = "em.plugins.config.which-key" },
-    { import = "em.plugins.config.comment" },
-    { import = "em.plugins.config.telescope" },
-    { import = "em.plugins.config.vim-dadbod" },
-    { import = "em.plugins.config.readline" },
-    { import = "em.plugins.config.zk-nvim" },
-    { import = "em.plugins.config.vim-gista" },
-    { import = "em.plugins.config.null-ls" },
-    { import = "em.plugins.config.lspconfig" },
-    { import = "em.plugins.config.lspsaga" },
-  },
-})
+require("lazy").setup("plugins")
