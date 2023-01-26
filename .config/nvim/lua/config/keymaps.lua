18b16531bc424681e87f64bec5d6447a02195775
Eric Milford <ericmilford@gmail.com>
2023-01-25 21:12:24 -0600

    (nvim) Move line movement keymaps
    General keymaps now live together in a single file.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,11 @@ vim.keymap.set("n", "<m-up>", "<cmd>resize +2<cr>", { desc = "Increase window he
 vim.keymap.set("n", "<m-down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 vim.keymap.set("n", "<m-left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 vim.keymap.set("n", "<m-right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+-- Move lines down/up using <option> + j/k
+vim.keymap.set("n", "<m-j>", [[:m .+1<cr>==]], { silent = true })
+vim.keymap.set("n", "<m-k>", [[:m .-2<cr>==]], { silent = true })
+vim.keymap.set("i", "<m-j>", [[<esc>:m .+1<cr>==gi]], { silent = true })
+vim.keymap.set("i", "<m-k>", [[<esc>:m .-2<cr>==gi]], { silent = true })
+vim.keymap.set("v", "<m-j>", [[:m '>+1<cr>gv=gv]], { silent = true })
+vim.keymap.set("v", "<m-k>", [[:m '<-2<cr>gv=gv]], { silent = true })
