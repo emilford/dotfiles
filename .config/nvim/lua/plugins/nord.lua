e910515e77ff6b03e7fd73ea449e7f751c246cea
Eric Milford <ericmilford@gmail.com>
2023-02-08 18:25:56 -0600

    (nvim) Use foreground mode for error highlight
    The default error highlight mode is unappealing and jarring.

--- .config/nvim/lua/plugins/nord.lua
+++ .config/nvim/lua/plugins/nord.lua
@@ -2,6 +2,7 @@ return {
   "gbprod/nord.nvim",
   priority = 1000,
   opts = {
+    errors = { mode = "fg" },
     on_highlights = function(highlights, colors)
       highlights["FidgetTitle"] = { fg = colors.frost.artic_water }
       highlights["FidgetTask"] = { fg = colors.snow_storm.origin }
