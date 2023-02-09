7d7dd47ddf83d69d06fa628c64fa6b05dc34ca88
Eric Milford <ericmilford@gmail.com>
2023-02-08 20:26:31 -0600

    (nvim) Fix indent-blankline treesitter option
    Lazy runs `config` after first loading the plugin. If the plugin needs
    the setting before loading, this is too late. In these cases use `init`
    to set the configuration before loading the plugin.

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,9 +1,10 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  config = function()
+  init = function()
     vim.g.indent_blankline_use_treesitter = true
-
+  end,
+  config = function()
     require("indent_blankline").setup()
   end,
 }
