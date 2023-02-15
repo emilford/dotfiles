5988d2eb70e71b60cd75cc3bbdd39ee8c47b2568
Eric Milford <ericmilford@gmail.com>
2023-02-14 18:54:35 -0600

    (nvim) Don't shrink quickfix window to results
    The default setting shrinks the quickfix window down to the number of
    results. This is hard to read when there aren't a lot results.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -18,6 +18,7 @@ return {
     event = "VeryLazy",
     init = function()
       vim.g.qf_auto_open_quickfix = 1
+      vim.g.qf_auto_resize = 0
       vim.g.qf_nowrap = 1
       vim.g.qf_shorten_path = 1
     end,
