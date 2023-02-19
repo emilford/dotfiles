4bfd2b7063465ca45fbdfc69f1baabc35022ce17
Eric Milford <ericmilford@gmail.com>
2023-02-19 14:16:52 -0600

    (nvim) Load zk-nvim if .zk is present
    When running Neovim in a directory that has a `zk` database initialized,
    load the zk-nvim plugin. Marks the plugin as `lazy=false` such that the
    `cond` setting determines whether to load the plugin.

--- .config/nvim/lua/plugins/zk-nvim.lua
+++ .config/nvim/lua/plugins/zk-nvim.lua
@@ -3,45 +3,40 @@ return {
   opts = {
     picker = "telescope",
   },
-  keys = function()
-    if require("zk.util").notebook_root(vim.fn.getcwd()) ~= nil then
-      return {
-        { "<cr>", "<cmd>lua vim.lsp.buf.definition()<cr>" },
-        { "K", "<cmd>lua vim.lsp.buf.hover()<cr>" },
-      }
-    end
+  lazy = false,
+  cond = function()
+    return vim.fn.isdirectory(".zk") ~= 0
   end,
   config = function(_, opts)
-    -- Add the key mappings only when in a zk notebook
-    if require("zk.util").notebook_root(vim.fn.getcwd()) ~= nil then
-      require("zk").setup(opts)
+    require("zk").setup(opts)
 
-      local wk = require("which-key")
+    vim.keymap.set("n", "<cr>", "<cmd>lua vim.lsp.buf.definition()<cr>", { buffer = 0 })
+    vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", { buffer = 0 })
 
-      wk.register({
-        z = {
-          name = "+ZettelKasten",
-          b = { "<cmd>ZkBacklinks<cr>", "Backlinks" },
-          o = { "<cmd>ZkNotes<cr>", "Open" },
-          f = { "<cmd>ZkNotes { dir = vim.fn.expand('%:p:h'), match = vim.fn.input('Find: ') }<cr>", "Find" },
-          i = { "<cmd>ZkIndex<cr>", "Index" },
-          l = { "<cmd>ZkLinks<cr>", "Links" },
-          n = { "<cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<cr>", "New" },
-          t = { "<cmd>ZkTags<cr>", "Tags" },
+    local wk = require("which-key")
+    wk.register({
+      z = {
+        name = "+ZettelKasten",
+        b = { "<cmd>ZkBacklinks<cr>", "Backlinks" },
+        o = { "<cmd>ZkNotes<cr>", "Open" },
+        f = { "<cmd>ZkNotes { dir = vim.fn.expand('%:p:h'), match = vim.fn.input('Find: ') }<cr>", "Find" },
+        i = { "<cmd>ZkIndex<cr>", "Index" },
+        l = { "<cmd>ZkLinks<cr>", "Links" },
+        n = { "<cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<cr>", "New" },
+        t = { "<cmd>ZkTags<cr>", "Tags" },
+      },
+    }, { prefix = "<leader>", mode = "n" })
+
+    wk.register({
+      z = {
+        name = "+ZettelKasten",
+        f = { ":'<'>ZkMatch<cr>", "Find" },
+        n = { ":'<'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<cr>", "New" },
+        N = {
+          ":'<'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') } <cr>",
+          "New",
         },
-      }, { prefix = "<leader>", mode = "n" })
-
-      wk.register({
-        z = {
-          name = "+ZettelKasten",
-          f = { ":'<'>ZkMatch<cr>", "Find" },
-          n = { ":'<'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<cr>", "New" },
-          N = {
-            ":'<'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') } <cr>",
-            "New",
-          },
-        },
-      }, { prefix = "<leader>", mode = "v" })
-    end
+      },
+    }, { prefix = "<leader>", mode = "v" })
   end,
 }
