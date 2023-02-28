f58776d16d0ca567f95ac921551400471beef4ae
Eric Milford <ericmilford@gmail.com>
2023-02-27 22:18:44 -0600

    (nvim) Use separate status line styling
    When `StatusLine` and `StatusLineNC` have the same styles Neovim appends
    carets to the status line of the current window to differentiate it from
    the rest. This looks no bueno. Instead, change the colors to a minimally
    different style to avoid this behavior.

--- .config/nvim/lua/plugins/rose-pine.lua
+++ .config/nvim/lua/plugins/rose-pine.lua
@@ -24,7 +24,7 @@ return {
         fg = "highlight_med",
       },
       StatusLine = {
-        bg = "surface",
+        bg = "highlight_low",
         fg = "muted",
       },
       StatusLineNC = {
