b3df3bc8d8a7ad07428b489d2949bcc64d77d79b
Eric Milford <ericmilford@gmail.com>
2023-03-09 15:58:23 -0600

    (nvim) Disable indent markers for more file types
    There's no reason to have indentation markers displayed for files of
    type `fugitive` or `gitcommit`. Append these file types to those the
    plugin already excludes.

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -5,6 +5,8 @@ return {
     vim.g.indent_blankline_use_treesitter = true
   end,
   config = function()
+    vim.g.indent_blankline_filetype_exclude =
+      vim.list_extend(vim.g.indent_blankline_filetype_exclude, { "fugitive", "gitcommit" })
     require("indent_blankline").setup()
   end,
 }
