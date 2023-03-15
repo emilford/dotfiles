48a57875b26b34411258106b349ae6bd39dbb12d
Eric Milford <ericmilford@gmail.com>
2023-03-15 14:34:52 -0500

    (nvim) Disable indent markers for more file types
    There's no reason to have indentation markers displayed for files of
    type `markdown`. Append these file types to those the plugin already
    excludes.

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -6,7 +6,7 @@ return {
   end,
   config = function()
     vim.g.indent_blankline_filetype_exclude =
-      vim.list_extend(vim.g.indent_blankline_filetype_exclude, { "fugitive", "gitcommit" })
+      vim.list_extend(vim.g.indent_blankline_filetype_exclude, { "fugitive", "gitcommit", "markdown" })
     require("indent_blankline").setup()
   end,
 }
