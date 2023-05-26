568e3f331fe692e0f697ec47941f90af085fa45b
Eric Milford <ericmilford@gmail.com>
2023-05-26 07:44:12 -0500

    (nvim) Move indent-blankline exlude files to opts
    The prior code tried to add to the default list of file types to exclude
    instead of replace it. This moves the exclude list to the opts table but
    requires adding back in the default file types added by the plugin.

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -2,11 +2,20 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPre", "BufNewFile" },
   opts = {
+    filetype_exclude = {
+      "",
+      "checkhealth",
+      "fugitive",
+      "gitcommit",
+      "help",
+      "lspinfo",
+      "man",
+      "markdown",
+      "packer",
+    },
     use_treesitter = true,
   },
   config = function(_plugin, opts)
-    vim.g.indent_blankline_filetype_exclude =
-      vim.list_extend(vim.g.indent_blankline_filetype_exclude, { "fugitive", "gitcommit", "markdown" })
     require("indent_blankline").setup(opts)
   end,
 }
