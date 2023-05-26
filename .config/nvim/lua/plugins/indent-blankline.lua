3f3402953b41788d0d68e1f0c17f994de9eae3ac
Eric Milford <ericmilford@gmail.com>
2023-05-26 07:37:20 -0500

    (nvim) Move indent-blankline treesitter to opts
    The `vim.g.indent_blankline_use_treesitter` will use `use_treesitter`
    when specified in the `setup` function. Moving this to `opts` has the
    same result as setting the global in `init`.

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,12 +1,12 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  init = function()
-    vim.g.indent_blankline_use_treesitter = true
-  end,
-  config = function()
+  opts = {
+    use_treesitter = true,
+  },
+  config = function(_plugin, opts)
     vim.g.indent_blankline_filetype_exclude =
       vim.list_extend(vim.g.indent_blankline_filetype_exclude, { "fugitive", "gitcommit", "markdown" })
-    require("indent_blankline").setup()
+    require("indent_blankline").setup(opts)
   end,
 }
