a0e4b4df7cca97221eadd1a1acf335892bc9987a
Eric Milford <ericmilford@gmail.com>
2023-07-26 20:43:09 -0500

    (nvim) Set local value for file type settings
    Changing settings using `vim.wo` sets the window-global value which has
    implications when splitting a window to open a new buffer. In this case
    splitting the window confusingly inherits the global value.
    
    While `vim.wo` uses a window global scope, using `opt_local` or similar
    sets just the local value and prevents inheritance of the changed value
    when splitting the window.
    
    This changes resolves issues with losing `number` settings when opening
    a file type with numbers after opening a file type without numbers. The
    issue meant needing to manually re-enable number settings.
    
    Note: it's unclear whether `vim.api.nvim_set_option_value` is the right
    API versus `opt_local` which appears to behave the same. Going with the
    former per the documentation and what `@folke` used in there update.
    
    See the following:
    
    `h: local-options`
    `https://github.com/folke/lazy.nvim/issues/829`
    `https://github.com/neovim/neovim/issues/14595`

--- .config/nvim/after/ftplugin/fugitive.lua
+++ .config/nvim/after/ftplugin/fugitive.lua
@@ -1,2 +1,2 @@
-vim.wo.number = false
-vim.wo.relativenumber = false
+vim.api.nvim_set_option_value("number", false, { scope = "local" })
+vim.api.nvim_set_option_value("relativenumber", false, { scope = "local" })
--- .config/nvim/after/ftplugin/gitcommit.lua
+++ .config/nvim/after/ftplugin/gitcommit.lua
@@ -1,6 +1,6 @@
 vim.bo.formatexpr = "format#Format()"
 vim.bo.textwidth = 72
-vim.wo.colorcolumn = "51"
-vim.wo.number = false
-vim.wo.relativenumber = false
-vim.wo.spell = true
+vim.api.nvim_set_option_value("colorcolumn", false, { scope = "local" })
+vim.api.nvim_set_option_value("number", false, { scope = "local" })
+vim.api.nvim_set_option_value("relativenumber", false, { scope = "local" })
+vim.api.nvim_set_option_value("spell", true, { scope = "local" })
