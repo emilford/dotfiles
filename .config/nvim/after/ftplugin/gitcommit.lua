cbcb3b79bbacf4e481883c849f7c7db6bf92775b
Eric Milford <ericmilford@gmail.com>
2023-07-26 22:04:44 -0500

    (nvim) Fix `colorcolumn` for gitcommit file type
    a0e4b4d changed the gitcommit file type settings to set the local value
    and in doing so mistakenly disabled the `colorcolumn` setting.

--- .config/nvim/after/ftplugin/gitcommit.lua
+++ .config/nvim/after/ftplugin/gitcommit.lua
@@ -1,6 +1,6 @@
 vim.bo.formatexpr = "format#Format()"
 vim.bo.textwidth = 72
-vim.api.nvim_set_option_value("colorcolumn", false, { scope = "local" })
+vim.api.nvim_set_option_value("colorcolumn", "51", { scope = "local" })
 vim.api.nvim_set_option_value("number", false, { scope = "local" })
 vim.api.nvim_set_option_value("relativenumber", false, { scope = "local" })
 vim.api.nvim_set_option_value("spell", true, { scope = "local" })
