4d0789346cb1dc8938d1a85a1b24029cdde0349e
Eric Milford <ericmilford@gmail.com>
2023-09-28 19:54:27 -0500

    (nvim) Use main branch of indent-blankline
    0db9190 switched to indent-blankline's `v3` branch to trial the latest
    release of the software. Now that v3 is in `main`, reference it there.

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,7 +1,6 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  branch = "v3",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = { "tpope/vim-sleuth" },
   opts = {
