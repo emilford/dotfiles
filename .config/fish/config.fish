b8022256d92a3cf27e5151695ed908bb44a32bca
Eric Milford <ericmilford@gmail.com>
2023-11-30 19:07:09 -0600

    (fish) Add Mason executables to PATH
    These include vale, etc. installed using Mason used on the command line.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,8 +15,9 @@ direnv hook fish | source
 set -x FZF_CTRL_T_COMMAND "fd --type file --follow --hidden --exclude .git"
 set -x FZF_DEFAULT_COMMAND $FZF_CTRL_T_COMMAND
 set -x RIPGREP_CONFIG_PATH $HOME/.config/ripgrep/ripgreprc
-set -x VISUAL "nvim"
+set -x VISUAL nvim
 
 fish_config theme choose "Rosé Pine Moon"
 
 fish_add_path -pP ".git/safe/../../bin"
+fish_add_path -aP "/Users/eric/.local/share/nvim/mason/bin"
