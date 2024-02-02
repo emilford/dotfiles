b5ac1c2d91fe94b7f2ef0f892466c171ffa79b45
Eric Milford <ericmilford@gmail.com>
2024-02-02 08:35:06 -0600

    (nvim) Don't show mode indicator for Insert, etc.
    This removes the message shown in the cmdline when in Insert, Replace,
    or Visual mode.

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -11,6 +11,7 @@ vim.opt.laststatus = 1
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ruler = false
+vim.opt.showmode = false
 vim.opt.shiftround = true
 vim.opt.shortmess:append("I")
 vim.opt.showmatch = true
