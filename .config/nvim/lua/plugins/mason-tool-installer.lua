7ccb1ef0201fb590de66e63895969fcf3dd614a5
Eric Milford <ericmilford@gmail.com>
2024-02-04 13:42:47 -0600

    (nvim) Fix mason-tool-installer `dependencies` key
    Fixes a typo that hadn't manifested as an issue.

--- .config/nvim/lua/plugins/mason-tool-installer.lua
+++ .config/nvim/lua/plugins/mason-tool-installer.lua
@@ -1,7 +1,7 @@
 return {
   "WhoIsSethDaniel/mason-tool-installer.nvim",
   event = "VeryLazy",
-  dependecies = {
+  dependencies = {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
   },
