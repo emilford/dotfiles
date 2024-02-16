fb3058e8c4a3a861bc11e17e7d667eed0cbd945d
Eric Milford <ericmilford@gmail.com>
2024-02-16 15:39:58 -0600

    (fish) Update theme switcher to restart Kitty
    Otherwise switching themes applies to the current window and not others.

--- .config/fish/functions/theme.fish
+++ .config/fish/functions/theme.fish
@@ -3,11 +3,13 @@ function theme --description "Switches theme to light or dark mode" --argument-n
         fish_config theme choose "Rosé Pine Dawn"
         cat "$HOME/.config/kitty/themes/rose-pine-dawn.conf" >"$HOME/.config/kitty/current-theme.conf"
         kitty @ set-colors -a "$HOME/.config/kitty/themes/rose-pine-dawn.conf"
+        kill -SIGUSR1 $KITTY_PID
         echo light >"$HOME/.theme"
     else if test $mode = dark
         fish_config theme choose "Rosé Pine Moon"
         kitty @ set-colors -a "$HOME/.config/kitty/themes/rose-pine-moon.conf"
         cat "$HOME/.config/kitty/themes/rose-pine-moon.conf" >"$HOME/.config/kitty/current-theme.conf"
+        kill -SIGUSR1 $KITTY_PID
         echo dark >"$HOME/.theme"
     end
 end
