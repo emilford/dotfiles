79d71750273eabd3ec7efc4d96fe15eba70a7986
Eric Milford <ericmilford@gmail.com>
2024-03-13 12:50:00 -0500

    (nvim) Add completion sources for gitcommit files
    Before gitcommit files used git and the buffer as completion sources.
    This extends the sources to also include snippets and LSP data.

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -103,6 +103,8 @@ return {
     cmp.setup.filetype("gitcommit", {
       sources = cmp.config.sources({
         { name = "git" },
+        { name = "luasnip" },
+        { name = "nvim_lsp" },
       }, {
         { name = "buffer" },
       }),
