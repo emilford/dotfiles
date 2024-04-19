d803c39e7fb90d016406e2e167f33c72b751363e
Eric Milford <ericmilford@gmail.com>
2024-04-19 10:51:28 -0500

    (nvim) Remove autocmd to disable format on save
    Neovim has `:noautocmd` (or `:noa`) for disabling autocmds allowing to
    disable conform's format on save with `:noa w`.

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -38,10 +38,6 @@ return {
       },
     },
     format_on_save = function(bufnr)
-      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-        return
-      end
-
       return {
         timeout_ms = 500,
         lsp_fallback = true,
@@ -66,18 +62,4 @@ return {
       desc = "Format Document",
     },
   },
-  init = function()
-    vim.api.nvim_create_user_command("FormatDisable", function(args)
-      if args.bang then
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
-    end, { desc = "Disable format on save", nargs = 0, bang = true })
-
-    vim.api.nvim_create_user_command("FormatEnable", function()
-      vim.b.disable_autoformat = false
-      vim.g.disable_autoformat = false
-    end, { desc = "Enable format on save", nargs = 0 })
-  end,
 }
