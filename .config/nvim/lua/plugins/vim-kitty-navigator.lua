9953d669302e20a4caf80dc56f511a497360f35a
Eric Milford <ericmilford@gmail.com>
2024-04-25 17:59:24 -0500

    (nvim) Load vim-kitty-navigator if in Kitty
    This otherwise interferes with the WezTerm navigation.

--- .config/nvim/lua/plugins/vim-kitty-navigator.lua
+++ .config/nvim/lua/plugins/vim-kitty-navigator.lua
@@ -1,5 +1,8 @@
 return {
   "knubie/vim-kitty-navigator",
+  cond = function()
+    return os.getenv("TERM") == "xterm-kitty"
+  end,
   event = { "BufReadPre", "BufNewFile" },
   build = "cp ./*.py ~/.config/kitty/",
 }
