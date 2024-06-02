13d83d41eeb372783e1c4679f77070b38dd4e01e
Eric Milford <ericmilford@gmail.com>
2024-06-02 12:48:50 -0500

    (nvim) Migrate Harpoon from v1 to v2
    Harpoon 2 gives greater control and will become the new stable release.

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,11 +1,59 @@
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
   keys = {
-    { "<leader>h", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show Harpoon List" },
-    { "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Add File to Harpoon List" },
-    { "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Open Harpoon File 1" },
-    { "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Open Harpoon File 2" },
-    { "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Open Harpoon File 3" },
-    { "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Open Harpoon File 4" },
+    {
+      "<leader>h",
+      function()
+        local harpoon = require("harpoon")
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = "Show Harpoon List",
+    },
+    {
+      "<leader>a",
+      function()
+        require("harpoon"):list():add()
+      end,
+      desc = "Add File to Harpoon List",
+    },
+    {
+      "<leader>1",
+      function()
+        require("harpoon"):list():select(1)
+      end,
+      desc = "Open Harpoon File 1",
+    },
+    {
+      "<leader>2",
+      function()
+        require("harpoon"):list():select(2)
+      end,
+      desc = "Open Harpoon File 2",
+    },
+    {
+      "<leader>3",
+      function()
+        require("harpoon"):list():select(3)
+      end,
+      desc = "Open Harpoon File 3",
+    },
+    {
+      "<leader>4",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Open Harpoon File 4",
+    },
+    {
+      "<leader>5",
+      function()
+        require("harpoon"):list():select(5)
+      end,
+      desc = "Open Harpoon File 5",
+    },
   },
 }
