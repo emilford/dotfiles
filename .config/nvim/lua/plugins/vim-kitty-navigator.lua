abed5159eefb2cf6dfe78058de1b07f080a9830f
Eric Milford <ericmilford@gmail.com>
2024-07-12 18:58:45 -0500

    (kitty) Remove remaining Kitty configuration
    Dropped Tmux (76bcebf) in favor of WezTerm (216d84a). The setup has
    worked well. Complete the switch and remove Kitty's configuration.

--- .config/nvim/lua/plugins/vim-kitty-navigator.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "knubie/vim-kitty-navigator",
-  cond = function()
-    return os.getenv("TERM") == "xterm-kitty"
-  end,
-  event = { "BufReadPre", "BufNewFile" },
-  build = "cp ./*.py ~/.config/kitty/",
-}
