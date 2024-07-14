b744e8ca347c8907f89c966b6f969cf7a66a6ea9
Eric Milford <ericmilford@gmail.com>
2024-07-14 10:33:05 -0500

    (fish) Remove Kitty from theme function
    abed515 removed Kitty.

--- .config/fish/functions/theme.fish
+++ .config/fish/functions/theme.fish
@@ -2,18 +2,8 @@ function theme --description "Switches theme to light or dark mode" --argument-n
     if test $mode = light
         echo light >"$HOME/.theme"
         fish_config theme choose "Rosé Pine Dawn"
-        cat "$HOME/.config/kitty/themes/rose-pine-dawn.conf" >"$HOME/.config/kitty/current-theme.conf"
-
-        if set -q KITTY_PID
-            kill -SIGUSR1 $KITTY_PID
-        end
     else if test $mode = dark
         echo dark >"$HOME/.theme"
         fish_config theme choose "Rosé Pine Moon"
-        cat "$HOME/.config/kitty/themes/rose-pine-moon.conf" >"$HOME/.config/kitty/current-theme.conf"
-
-        if set -q KITTY_PID
-            kill -SIGUSR1 $KITTY_PID
-        end
     end
 end
