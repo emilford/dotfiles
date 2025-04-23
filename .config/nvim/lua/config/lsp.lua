bead40b44449be6b17556f07d8e881745e7ac22f
Eric Milford <ericmilford@gmail.com>
2025-04-22 22:10:51 -0400

    (nvim) Add ESLint language server
    For editing JavaScript files.

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -13,4 +13,4 @@ vim.diagnostic.config({
 })
 
 vim.lsp.config("*", { root_markers = { ".git" } })
-vim.lsp.enable({ "jsonls", "lua_ls", "ruby_lsp", "stimulus_ls", "vale_ls", "yamlls" })
+vim.lsp.enable({ "eslint_lsp", "jsonls", "lua_ls", "ruby_lsp", "stimulus_ls", "vale_ls", "yamlls" })
