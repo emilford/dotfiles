7a310af463d5cec40e1e1c2d7bc750b94571d498
Eric Milford <ericmilford@gmail.com>
2025-05-10 00:02:38 -0400

    (nvim) Format JavaScript files with Prettier
    Format JavaScript family of files with Prettier when configured.

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -4,8 +4,16 @@ return {
     cmd = { "ConformInfo" },
     event = { "BufWritePre" },
     opts = {
+      formatters = {
+        prettierd = { require_cwd = true },
+      },
       formatters_by_ft = {
         ["lua"] = { "stylua" },
+        ["javascript"] = { "prettierd" },
+        ["javascriptreact"] = { "prettierd" },
+        ["jsx"] = { "prettierd" },
+        ["typescript"] = { "prettierd" },
+        ["typescriptreact"] = { "prettierd" },
       },
       format_on_save = {
         lsp_format = "fallback",
@@ -16,6 +24,7 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = function(_, opts)
       opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, {
+        "prettierd",
         "stylua",
       })
     end,
