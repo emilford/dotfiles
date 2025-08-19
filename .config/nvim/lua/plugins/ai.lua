a84749bb98b17cef15fdf69b8b842e8708b92c76
Eric Milford <ericmilford@gmail.com>
2025-08-19 14:28:58 -0400

    (nvim) Add opencode.nvim

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -80,4 +80,61 @@ return {
     cmd = "MCPHub",
     config = true,
   },
+  {
+    "NickvanDyke/opencode.nvim",
+    keys = {
+      {
+        "<leader>oa",
+        function()
+          require("opencode").ask()
+        end,
+        desc = "Ask opencode",
+        mode = "n",
+      },
+      {
+        "<leader>oa",
+        function()
+          require("opencode").ask("@selection: ")
+        end,
+        desc = "Ask opencode about selection",
+        mode = "v",
+      },
+      {
+        "<leader>op",
+        function()
+          require("opencode").select_prompt()
+        end,
+        desc = "Select prompt",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>on",
+        function()
+          require("opencode").command("session_new")
+        end,
+        desc = "New session",
+      },
+      {
+        "<leader>oy",
+        function()
+          require("opencode").command("messages_copy")
+        end,
+        desc = "Copy last message",
+      },
+      {
+        "<S-C-u>",
+        function()
+          require("opencode").command("messages_half_page_up")
+        end,
+        desc = "Scroll messages up",
+      },
+      {
+        "<S-C-d>",
+        function()
+          require("opencode").command("messages_half_page_down")
+        end,
+        desc = "Scroll messages down",
+      },
+    },
+  },
 }
