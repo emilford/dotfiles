692bc2de997b9077e946a1fe82484ec462af6581
Eric Milford <ericmilford@gmail.com>
2025-11-11 22:08:05 -0500

    Remove unused stuff

--- .install/brew.sh
+++
@@ -1,9 +0,0 @@
-header "brew"
-
-if [[ $(command -v brew) == "" ]]; then
-  run "brew install" '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
-else
-  noop "brew install"
-fi
-
-run "brew bundle" "/opt/homebrew/bin/brew bundle --file=$HOME/.brewfile"
--- .install/neovim.sh
+++
@@ -1,3 +0,0 @@
-header "Neovim"
-
-run "install plugins" "/opt/homebrew/bin/nvim --headless +Lazy! sync +q"
